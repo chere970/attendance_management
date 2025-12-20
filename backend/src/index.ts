af4586aa073bc4cace2ca486d1e1001f1f074417
cherinet <cherinetkebede055@gmail.comg>
2025-12-20 17:56:14 +0300

    Refactor CORS configuration and update TypeScript settings for improved compatibility

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,8 +27,9 @@ const prisma = new PrismaClient({
 
 app.use(cors({
   origin: [
-    "http://localhost:3000",
-    "https://attendance-management.vercel.app"
+    // "http://localhost:3000",
+    "https://attendance-management.vercel.app",
+    "https://attendance-management-2y8y.vercel.app/"
   ],
   credentials: true
 }));
